635856928b451793ead0d687c7eaba45053a92a1
josepablito16 <josepablocif16@gmail.com>
2019-08-08 00:17:14 -0600

    se agrego script inicial de movimiento y de camara

---
+++ UVGame/Assets/TucTuc.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TucTuc : MonoBehaviour
+{
+    public float force = 65f;
+    private Vector3 moveTo;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        moveTo = new Vector3(horizontalMovement, 0, verticalMovement);
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveTo.magnitude > 0.1)
+        {
+            rb.AddForce(force * moveTo);
+            transform.forward = -moveTo;
+        }
+    }
+}
